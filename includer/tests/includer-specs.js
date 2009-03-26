59e5fae715d5489a908fba85d847c9bc542e3d5e
Daniel Tabuenca <dan@eleutian.com>
2009-03-26 00:41:04 -0700

    Setup project skeleton and testing framework

---
+++ includer/tests/includer-specs.js
@@ -0,0 +1,10 @@
+Screw.Unit(function(){  
+  describe("First Spec",function(){
+    before(function(){
+    });
+
+    it("should pass",function(){
+      expect('blah').to(equal,'blah');
+    });
+  });
+});
