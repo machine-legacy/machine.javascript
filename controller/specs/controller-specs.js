c601b39951ee34f8843c39ef2c7c3d7bf0d4377c
Daniel Tabuenca <dan@eleutian.com>
2009-11-20 02:16:34 -0800

    Started some specs.

---
+++ controller/specs/controller-specs.js
@@ -0,0 +1,78 @@
+(function(){
+
+
+Screw.Unit(function(){  
+  function MyController(){
+  }
+
+  MyController.prototype = new Machine.Controller();
+  var controller;
+
+
+
+  describe("View Rendering",function(){
+    before(function(){
+     controller = new MyController(); 
+    });
+
+    describe("When no renderer specified",function(){
+      var myView = "<div>THIS IS MY VIEW $VAR</div>";
+      before(function(){
+        controller.setView(myView);
+        controller.render();
+      });
+
+      it("Should default to dumb renderer that simply returns the view", function(){
+        expect(controller.domRoot.html()).to(equal, $(myView).html());   
+      });
+      
+      describe("When custom default renderer was registered", function(){
+        var usedMyDefault = false;
+        function MyDefaultRenderer(view){
+          this.render = function(controller, model){
+           usedMyDefault = true;
+           return $("<div></div>");
+          };
+        }
+        before(function(){
+          Machine.Controller.registerViewRenderer("default", MyDefaultRenderer);
+          controller.setView(myView);
+          controller.render();
+        });
+
+        it("Should use the custom one instead of the original default",function(){
+          expect(usedMyDefault).to(be_true);
+        });
+
+
+      });
+    });
+    describe("When custom renderer was registered", function(){
+        var usedMyCustom = false;
+        function MyCustomRenderer(view){
+          this.render = function(controller, model){
+           usedMyCustom = true;
+           return $("<div></div>");
+          };
+        }
+
+        before(function(){
+          Machine.Controller.registerViewRenderer("custom", MyCustomRenderer);
+        });
+
+        describe("When view is set to render with custom renderer", function(){
+          before(function(){
+            controller.setView("anything", "custom");
+            controller.render();
+          });
+
+          it("Should use the defined renderer",function(){
+            expect(usedMyCustom).to(be_true);
+          });
+        });
+
+      });
+    });
+});
+
+}());
