119ea79b4ac978e30b2dfd4a0c6fcacbb766108d
Jeffery Olson <olson.jeffery@gmail.com>
2009-11-25 06:40:24 -0800

    added a spec to verify case-insensivity for renderer name registration

--- controller/specs/controller-specs.js
+++ controller/specs/controller-specs.js
@@ -73,6 +73,30 @@ Screw.Unit(function(){
 
       });
     });
+
+    describe("View Reigstration", function() {
+      function FooViewRenderer(view) { }
+      FooViewRenderer.prototype.render = function(ctlr, model) { return "<div id='foo'></div>"; }
+
+      var controller;
+
+
+      describe("When a controller calls setView with 'Foo' as the renderer name when the renderer was globally registered with the name 'foo'", function() {
+
+        before(function() {
+          Machine.Controller.registerViewRenderer("Foo", FooViewRenderer);
+  
+          controller = new MyController();
+          controller.setView("", "foo");
+          
+          controller.render();
+        });
+        
+        it("should treat renderer names as case-insensitive and wire the renderer up to the controller", function() {
+          expect($(controller.domRoot).attr('id')).to(equal, "foo");
+        });
+      });
+    });
 });
 
 }());
